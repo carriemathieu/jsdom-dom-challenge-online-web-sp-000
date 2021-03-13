98e41b6636edf817bb7c0a1a1b2e4b166ee51281
Carrie Mathieu <carrieburks@gmail.com>
2021-03-12 20:01:18 -0500

    commit tree 2d1c950154e0983fa825b75006409d22194a562b

---
+++ js/challenge.js
@@ -0,0 +1,15 @@
+/* As a user, I should see the timer increment every second once the page has loaded. */
+    /* hint for timer : setTimeout, setInterval, clearinterval */
+
+/* As a user, I can manually increment and decrement the counter using the plus and minus buttons. */
+
+/* As a user, I can 'like' an individual number of the counter. I should see the count of the number of 'likes' associated with that number displayed. */
+
+/* As a user, I can pause the counter, which should:
+    pause the counter
+    disable all buttons except the pause button
+    switch the label on the button from "pause" to "resume" */
+
+/* As a user, I should be able to click the "restart" button to restart the counter and re-enable the buttons. */
+
+/* As a user, I can leave comments on my gameplay, such as: "Wow, what a fun game this is." */
